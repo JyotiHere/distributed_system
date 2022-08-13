e1f214bf11f1b5e7d760b77252f8acaf117f31c7
JyotiHere <102534757+JyotiHere@users.noreply.github.com>
2022-08-13 02:04:15 +0100

    Commit 2

--- SmartWarehouse/src/main/java/warehouse_CA/warehouseControl/WarehouseControlServer.java
+++ SmartWarehouse/src/main/java/warehouse_CA/warehouseControl/WarehouseControlServer.java
@@ -1,7 +1,14 @@
 package warehouse_CA.warehouseControl;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.util.Properties;
 import java.util.logging.Logger;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
 
 import warehouse_CA.warehouseControl.LightResponse;
 import warehouse_CA.automateOrders.OrderDetailsRequest;
@@ -24,9 +31,15 @@ public class WarehouseControlServer extends warehouseControlImplBase {
 				
 				 // The StringServer is the current file name/ class name. Using an instance of this class different methods could be invoked by the client.
 				 WarehouseControlServer warehousecontrolserver = new WarehouseControlServer();
+				 
+				 Properties prop = warehousecontrolserver.getProperties();
+					
+				 warehousecontrolserver.registerService(prop);
+					
+					int port = Integer.valueOf( prop.getProperty("service_port") );
 				   
 				 // This is the port number where server will be listening to clients. Refer - https://en.wikipedia.org/wiki/Port_(computer_networking)	
-				    int port = 50057;
+				    //int port = 50057;
 				    
 				 // Here, we create a server on the port defined in in variable "port" and attach a service "stringserver" (instance of the class) defined above.
 				    Server server = ServerBuilder.forPort(port) // Port is defined in line 34
@@ -40,6 +53,71 @@ public class WarehouseControlServer extends warehouseControlImplBase {
 				    // Server will be running until externally terminated.
 				    server.awaitTermination();
 			 }
+			 
+			 
+			//jmDNS 
+			 private Properties getProperties() {
+					
+					Properties prop = null;		
+					
+					 try (InputStream input = new FileInputStream("src/main/resources/warehouseControl.properties")) {
+
+				            prop = new Properties();
+
+				            // load a properties file
+				            prop.load(input);
+
+				            // get the property value and print it out
+				            System.out.println("Warehouse Control Service properties ...");
+				            System.out.println("\t service_type: " + prop.getProperty("service_type"));
+				            System.out.println("\t service_name: " +prop.getProperty("service_name"));
+				            System.out.println("\t service_description: " +prop.getProperty("service_description"));
+					        System.out.println("\t service_port: " +prop.getProperty("service_port"));
+
+				        } catch (IOException ex) {
+				            ex.printStackTrace();
+				        }
+				
+					 return prop;
+				}
+				
+				
+				private  void registerService(Properties prop) {
+					
+					 try {
+				            // Create a JmDNS instance
+				            JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+				            
+				            String service_type = prop.getProperty("service_type") ;//"_http._tcp.local.";
+				            String service_name = prop.getProperty("service_name")  ;// "example";
+				           // int service_port = 1234;
+				            int service_port = Integer.valueOf( prop.getProperty("service_port") );// #.50051;
+
+				            
+				            String service_description_properties = prop.getProperty("service_description")  ;//"path=index.html";
+				            
+				            // Register a service
+				            ServiceInfo serviceInfo = ServiceInfo.create(service_type, service_name, service_port, service_description_properties);
+				            jmdns.registerService(serviceInfo);
+				            
+				            System.out.printf("registrering service with type %s and name %s \n", service_type, service_name);
+				            
+				            // Wait a bit
+				            Thread.sleep(1000);
+
+				            // Unregister all services
+				            //jmdns.unregisterAllServices();
+
+				        } catch (IOException e) {
+				            System.out.println(e.getMessage());
+				        } catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+				    
+				}
+			 
+			 
 	
 	//Override methods
 		
